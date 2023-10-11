eada88cfb6f223c26365a55b6ffed14c919cc0b6
Gonbriel <gonbriel03@gmail.com>
2023-10-10 21:21:25 -0300

    Criando a função string dos jogadores e criando a função de adicionar e deletar jogadores nos torneios

--- marica_chess_program.py
+++ marica_chess_program.py
@@ -11,9 +11,22 @@ class Players():
         self.rating = rating
         self.clube = clube
 
+    def __str__(self):
+        return f'Nome: {self.nome}, Idade: {self.idade}, Rating: {self.rating}, Clube: {self.clube}'
+
 class Torneios():
     def __init__(self, nome, tipo, data, local):
         self.nome = nome
         self.tipo = tipo
         self.data = data
         self.local = local
+        self.jogadores = []
+
+    def add_player(self, jogador):
+        self.jogadores.append(jogador)
+
+    def delete_player(self, jogador):
+        if jogador in self.jogadores:
+            self.jogadores.remove(jogador)
+        else:
+            print(f"O jogador {jogador.nome} não está na lista de jogadores deste torneio.")
