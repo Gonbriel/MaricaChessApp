0a45df97095724b4a4c1e3409f6ff284eb4b56be
Gonbriel <gonbriel03@gmail.com>
2023-10-19 19:00:58 -0300

    Criando propriedades e opções de mudanças. Modificando a função de deletar jogador de torneio.

--- marica_chess_program.py
+++ marica_chess_program.py
@@ -4,6 +4,8 @@
    2 - Cadastro de jogadores
    3 - Cálculo de rating                                  """
 
+import re
+
 class Players():
     def __init__(self, nome, idade, email, telefone, rating, clube):
         self._nome = nome.title()
@@ -11,7 +13,61 @@ class Players():
         self._email = email
         self._telefone = telefone
         self._rating = rating
-        self._clube = clube
+        self._clube = clube.upper()
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+    @property
+    def idade(self):
+        return self._idade
+
+    @idade.setter
+    def idade(self, nova_idade):
+        if isinstance(nova_idade, int) and nova_idade > 0:
+            self._idade = nova_idade
+        else:
+            raise ValueError('A idade deve ser um número inteiro positivo')
+
+    @property
+    def email(self):
+        return self._email
+
+    @email.setter
+    def email(self, novo_email):
+        if re.match(r"[^@]+@[^@]+\.[^@]+", novo_email):
+            self._email = novo_email
+        else:
+            raise ValueError("O e-mail deve estar em um formato válido.")
+
+    @property
+    def telefone(self):
+        return self._telefone
+
+    @email.setter
+    def email(self, novo_telefone):
+        self._telefone = novo_telefone
+
+    @property
+    def rating(self):
+        return self._rating
+
+    @rating.setter
+    def rating(self, novo_rating):
+        self._rating = novo_rating
+
+    @property
+    def clube(self):
+        return self._clube
+
+    @clube.setter
+    def clube(self, novo_clube):
+        self._clube = novo_clube.upper()
 
     def __str__(self):
         return f'Nome: {self._nome}, Idade: {self._idade}, Rating: {self._rating}, Clube: {self._clube}'
@@ -28,8 +84,7 @@ class Torneios():
         self.jogadores.append(jogador)
 
     def delete_player(self, jogador):
-        if jogador in self.jogadores:
+        try:
             self.jogadores.remove(jogador)
-        else:
-            print(f"O jogador {jogador.nome} não está na lista de jogadores deste torneio.")
-
+        except ValueError:
+            raise ValueError(f"O jogador {jogador.nome} não está na lista de jogadores deste torneio.")
